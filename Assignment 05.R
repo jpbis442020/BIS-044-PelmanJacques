6bc63403da0171b13e3caa60c3ecc535d3431bc9
Jacques pelman <jqp222@lehigh.edu>
2020-11-05 20:03:06 -0500

    hw5 update

--- Assignment 05.R
+++ Assignment 05.R
@@ -4,20 +4,15 @@ View(trendingdatecol)
 if (!require("tidyverse")) install.packages("tidyverse")
 library(tidyverse)
 DF1 <- df %>% separate(col=publish_time,into=c("PDate", "PTime"),"T",extra="merge")
+timecol2 <- timecol %>% separate(col=`DF1$PTime`,into=c("PTimee"), "000")
+view(timecol2)
 View(DF1)
+
 datecol<-as.data.frame(DF1$PDate)
 View(datecol)
 timecol<-as.data.frame(DF1$PTime)
 View(timecol)
-#str_to_title(timecol)
-#View(timecol)
-#df %>% str_replace("title", "Title")
-#dfdone <- df %>%
-#  str_replace_all("title", "Title")
-#view(dfdone)
 
-#DF_replaced <- mutate(df,title=replace(title,title=="title","Title"))
-#View(DF_replaced)
 DF_replaced <- mutate(df,title=str_to_title(title,title=="title"))
 View(DF_replaced)
 
